b937dc753055301654c9244bb9ec925ed986f500
rihardsklamanis <78017511+rihardsklamanis@users.noreply.github.com>
2021-03-23 16:21:59 +0200

    6 uzd

--- 06-detecting-typed-input/global.js
+++ 06-detecting-typed-input/global.js
@@ -1,1 +1,10 @@
 // Your JavaScript goes here.
+window.addEventListener('load', function () {
+
+    var htmlname = document.querySelector('#greeting')
+    const namebar = document.querySelector('#full_name')
+    namebar.addEventListener('keyup', function (e) {
+        htmlname.textContent = "Hello " + e.target.value + "!"
+    })
+
+})
