37d348bb8fbbccaed9c2d3e0de6ecc248c9834b5
rihardsklamanis <78017511+rihardsklamanis@users.noreply.github.com>
2021-03-23 16:13:57 +0200

    5 uzd

--- 05-toggle-elements-of-dom-path/global.js
+++ 05-toggle-elements-of-dom-path/global.js
@@ -1,1 +1,21 @@
 // Your JavaScript goes here.
+  
+window.addEventListener('load', function () {
+
+    const elements = document.querySelectorAll(".second_five li")
+    const button = document.querySelector('#toggle_button')
+
+    var togglestat = false
+    button.addEventListener('click', function (e) {
+        element.forEach(function (element) {
+            if (togglestat == false) {
+                element.style.display = 'flex'
+            }
+            else
+            {
+                element.style.display = 'none'
+            }
+        })
+        togglestat = !togglestat
+    })
+})   
