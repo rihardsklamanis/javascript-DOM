b2baeb94f678ff8c11face4d41512cb4537258ff
rihardsklamanis <78017511+rihardsklamanis@users.noreply.github.com>
2021-03-23 16:27:43 +0200

    7 uzdevums

--- 07-automatic-tabbing-fields/global.js
+++ 07-automatic-tabbing-fields/global.js
@@ -1,1 +1,21 @@
 // Your JavaScript goes here.
+  
+window.addEventListener('load', function () {
+
+    const first = document.querySelector('#first')
+    const second = document.querySelector('#second')
+    const third = document.querySelector('#third')
+    first.addEventListener('keydown', function (e) {
+        if (first.value.length == 3)
+        {
+            second.focus()
+        }
+    })
+    second.addEventListener('keydown', function (e) {
+        if (second.value.length == 3)
+        {
+            third.focus()
+        }
+    })
+
+})
