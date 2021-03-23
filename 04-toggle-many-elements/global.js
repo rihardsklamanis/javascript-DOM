bae22b90681375c29ecf85054f862c8557fcbd03
rihardsklamanis <78017511+rihardsklamanis@users.noreply.github.com>
2021-03-23 15:48:30 +0200

    4 uzd

--- 04-toggle-many-elements/global.js
+++ 04-toggle-many-elements/global.js
@@ -1,1 +1,21 @@
 // Your JavaScript goes here.
+window.addEventListener("load", function () {
+
+    const Element = document.querySelectorAll('.toggle_me')
+    const button = document.querySelector('#button')
+
+    var toggleStatus = false
+    button.addEventListener('click', function(e) {
+        if (toggleStatus == false){
+            Element.forEach(function(element) {
+                element.style.display = 'initial'
+                toggleStatus = true
+            })
+        }else{
+            Element.forEach(function (element) {
+                element.style.display = 'none'
+                toggleStatus = false
+            })
+        }
+    })
+})
