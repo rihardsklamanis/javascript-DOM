d5497d06cc812a1014331a5f31944f6322460089
rihardsklamanis <78017511+rihardsklamanis@users.noreply.github.com>
2021-03-23 14:16:54 +0200

    1 uzd

--- 01-show-one-element/global.js
+++ 01-show-one-element/global.js
@@ -1,17 +1,10 @@
 window.addEventListener("load", function(){
-
-  // Here is some pseudo-code to help you get started:
-
-  // 1. Get the DOM element which will be clicked.
-
-  // 2. Add a listener for the 'click' event onto that element.
-
-  // 3. The block for the listener should get the DOM
-  //    element containing the text to reveal.
-
-  // 4. Modify that DOM element's style to change it's 'display'
-  //    from a hidden value to a shown value.
-
-  // 5. Also modify the DOM to hide the "More info..." link.
+  const button = document.querySelector('#more_text_link')
+  const moretext = document.querySelector('#more_text_content')
+  moretext.style.display = 'none'
+button.addEventListener('click', function(e){
+  moretext.style.display = 'initial'
+  button.style.display = 'none'
+})
   
 });
